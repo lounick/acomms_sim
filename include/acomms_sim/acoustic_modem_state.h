abdae787da95dcf5147a49efade93510227362df
Nick Tsiogkas <nt95@hw.ac.uk>
2014-07-25 17:17:47 +0100

    Added methods.

--- include/acomms_sim/acoustic_modem_state.h
+++ include/acomms_sim/acoustic_modem_state.h
@@ -9,16 +9,46 @@
 #define ACOUSTIC_MODEM_STATE_H_
 
 #include <vector>
+#include <set>
 #include <ros/ros.h>
 #include <acomms_sim/AcousticModemData.h>
+#include <acomms_sim/acoustic_packet_state.h>
+#include <acomms_sim/time_interval.h>
 
 namespace acomms_sim
 {
 
+struct classcomp {
+  bool operator() (const acomms_sim::TimeInterval& lhs, const acomms_sim::TimeInterval& rhs) const
+  {return lhs._endTime > rhs._endTime;}
+};
+
 class AcousticModemState
 {
 protected:
+  ros::NodeHandlePtr _nh;
+  ros::Subscriber _inboundSub;
+  ros::Publisher _outboundPub;
+  std::string _platformName;
+  int _slot;
+  std::set<acomms_sim::AcousticPacketState> _packetSet;
+  std::set<acomms_sim::TimeInterval, classcomp> _modemBusySet;
 
+public:
+  AcousticModemState(ros::NodeHandle *nh, std::string platformName, int slot, std::string inboundTopic, std::string outboundTopic);
+  int getSlot();
+  std::string getName();
+  std::string getInboundTopic();
+  std::string getOutboundTopic();
+  //ros::Publisher<AcousticModemData>* getOutboundPub();
+  void callback(AcousticModemData msg);
+  void publishMessage(AcousticModemData msg);
+  void call(AcousticModemData request);
+  std::set<AcousticPacketState> getPacketState();
+  bool addSendPacket(AcousticModemData msg, AcousticPacketState packet);
+  AcousticPacketState addReceivePacker(AcousticPacketState sendPacket, long flightTime);
+  std::vector<AcousticPacketState> getNonCollidedReceivedPackets(long updateTime);
+  void removeOldModemBusyIntervals(long updateTime);
 };
 
 }  // namespace acomms_sim
--- src/acoustic_modem_state.cpp
+++ src/acoustic_modem_state.cpp
@@ -5,6 +5,81 @@
  *      Author: nick
  */
 
+#include <acomms_sim/acoustic_modem_state.h>
 
+namespace acomms_sim
+{
+AcousticModemState::AcousticModemState(ros::NodeHandle *nh, std::string platformName, int slot, std::string inboundTopic, std::string outboundTopic)
+{
+  _nh = nh;
+  _platformName = platformName;
+  _slot = slot;
+  _inboundSub = nh->subscribe(inboundTopic, 5, callback);
+  _outboundPub = nh->advertise<AcousticModemData>(outboundTopic, 5);
+}
 
+int AcousticModemState::getSlot()
+{
+  return _slot;
+}
 
+std::string AcousticModemState::getName()
+{
+  return _platformName;
+}
+
+std::string AcousticModemState::getInboundTopic()
+{
+  return _inboundSub.getTopic();
+}
+
+std::string AcousticModemState::getOutboundTopic()
+{
+  return _outboundPub.getTopic();
+}
+
+//ros::Publisher<AcousticModemData>* AcousticModemState::getOutboundPub() //TODO: Probably not correct. Should implement a member  function to access the publisher. Maybe return a pointer?
+//{
+//
+//}
+
+void AcousticModemState::callback(AcousticModemData msg)
+{
+  //TODO: Implement callback.
+}
+
+void AcousticModemState::publishMessage(AcousticModemData msg)
+{
+  _outboundPub.publish(msg);
+}
+
+void AcousticModemState::call(AcousticModemData request)
+{
+  //TODO: What do we need this for???
+}
+
+std::set<AcousticPacketState> AcousticModemState::getPacketState()
+{
+  return _packetSet;
+}
+
+bool AcousticModemState::addSendPacket(AcousticModemData msg, AcousticPacketState packet)
+{
+
+}
+
+AcousticPacketState AcousticModemState::addReceivePacker(AcousticPacketState sendPacket, long flightTime)
+{
+
+}
+
+std::vector<AcousticPacketState> AcousticModemState::getNonCollidedReceivedPackets(long updateTime)
+{
+
+}
+
+void AcousticModemState::removeOldModemBusyIntervals(long updateTime)
+{
+
+}
+}  // namespace accoms_sim
